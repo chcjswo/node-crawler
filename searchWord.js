d7ccc0f442f99d02aa271f5dca5fff034fefc1d3
tony <chcjswo@gmail.com>
2019-04-08 01:28:37 +0900

    nate 순위 추가 작업중

--- searchWord.js
+++ searchWord.js
@@ -1,95 +1,13 @@
-const client = require('cheerio-httpcli');
 const request = require('request');
-
 const naver = require('./sites/naver');
 const melon = require('./sites/melon');
 const daum = require('./sites/daum');
 const zum = require('./sites/zum');
-
-const results = [];
+const nate = require('./sites/nate');
 
 const HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
 };
-
-const searchUrl = [
-    'https://www.naver.com',
-    // 'http://search.daum.net/nate?thr=sbma&w=tot&q=b',
-    'http://www.nate.com/nate/LiveKeyword',
-    'https://www.daum.net',
-    'http://zum.com/#!/home',
-];
-const tag = [
-    '.ah_roll_area  > .ah_l > .ah_item > a > .ah_k',
-    'div#keywordRank > div.kwd_list',
-    // '#ratIssueColl > div.coll_cont > div > ol',
-    // '#mArticle > div.cmain_tmp > div.section_media > div.hotissue_builtin > div.realtime_part > ol > li:nth-child(1) > div > div:nth-child(1) > span.txt_issue > a > .link_issue',
-    'ol.list_hotissue.issue_row.list_mini > li > div > div > span.txt_issue > a.link_issue',
-    '.issue_keyword.d_rank_keyword > ul > li > .d_btn_keyword.d_ready',
-];
-const queryUrl = [
-  'https://search.naver.com/search.naver?where=nexearch&sm=top_lve&ie=utf8&query=',
-  'https://search.daum.net/nate?w=tot&q=',
-  'https://search.daum.net/search?w=tot&DA=ATG&rtmaxcoll=1TH&q=',
-  'http://search.zum.com/search.zum?qm=g_real1.top&real1_id=1&method=uni&option=accu&query=',
-];
-
-// const searchCallback = [
-//     naverSearch,
-//     naverSearch,
-//     naverSearch,
-//     naverSearch
-// ];
-
-// const searching = (url, tag, searchUrl) => {
-//     client.fetch(url, (err, $, res) => {
-//         if (err) {
-//             console.log(err);
-//             return;
-//         }
-//         const search = $(tag);
-//         console.log(search.length);
-//         // console.log('search ===> ', search);
-//
-//         search.each((index, item) => {
-//             if (index > 9) {
-//                 return false;
-//             }
-//
-//             const rank = index + 1;
-//             const text = $(item).text().replace(/ /gi, '');;
-//             // console.log('item ===> ', $(item));
-//
-//             // console.log(`${rank} - ${text} - ${searchUrl}${encodeURI(text)}`);
-//             console.log('item ===> ', $(item).attr('href'));
-//             // console.log('item ===> ', $(item).text());
-//             // console.log('item ===> ', $(item).href);
-//             // console.log(`${index + 1} - ${$(item).text()}`);
-//             // console.log(`${searchUrl}${encodeURI($(item).text())}`);
-//
-//         });
-//     });
-// };
-// const nateSearching = (url, tag, searchUrl) => {
-//     const options = {
-//         url,
-//         headers: {
-//             'User-Agent': 'request'
-//         },
-//         encoding: 'utf-8'
-//     };
-//
-//     const callback = (error, response, body) => {
-//         if (!error && response.statusCode === 200) {
-//             const data = iconv.decode(body, 'utf-8');
-//             console.log(data);
-//         }
-//     }
-//
-//     request(options, callback);;
-// };
-
-main();
 
 function search(url, callback)  {
     const options = {
@@ -108,33 +26,23 @@ function search(url, callback)  {
     });
 };
 
-async function main()  {
-    // search(searchUrl[0], searchCallback[0]);
-    // search(searchUrl[0], searchCallback[0])
-    //     .then(result => {
-    //         // console.log(result);
-    //
-    //         console.log(results);
-    //     });
-    // naver((err, ret) => {
-    //     if (err) throw err;
-    //
-    //     console.log(results);
-    //     console.log('aaaa');
-    // });
-
+async function exec()  {
     const naverList = await search(naver.searchUrl, naver.search);
     const melonList = await search(melon.searchUrl, melon.search);
     const daumList = await search(daum.searchUrl, daum.search);
     const zumList = await search(zum.searchUrl, zum.search);
+    const nateList = await search(nate.searchUrl, nate.search);
 
-    console.log(naverList);
-    console.log(melonList);
-    console.log(daumList);
-    console.log(zumList);
+    // console.log(naverList);
+    // console.log(melonList);
+    // console.log(daumList);
+    // console.log(zumList);
+    console.log(nateList);
 
     // searching(searchUrl[0], tag[0], queryUrl[0]);
     // nateSearching(searchUrl[1], tag[1], queryUrl[1]);
     // searching(searchUrl[2], tag[2], queryUrl[2]);
     // searching(searchUrl[3], tag[3], queryUrl[3]);
 }
+
+exec();
--- sites/nate.js
+++ sites/nate.js
@@ -1,25 +1,23 @@
 const cheerio = require('cheerio');
 
-const searchUrl = 'https://www.naver.com';
-const queryUrl = 'https://search.naver.com/search.naver?where=nexearch&sm=top_lve&ie=utf8&query=';
+const searchUrl = 'https://www.nate.com/nate/LiveKeyword';
 
 const search = (error, response, body) => {
+    // console.log(response);
     if (!error && response.statusCode === 200) {
+        // console.log(body);
         // 결과 정보 배열
         const results = [];
-        const $ = cheerio.load(body);
+        const $ = cheerio.load(body, {decodeEntities: true});
         // 검색 결과 태그
-        const searchTag = $('.ah_roll_area  > .ah_l > .ah_item > a > .ah_k');
+        const searchTag = $('html');
 
         searchTag.each((index, item) => {
-            if (index > 9) {
-                return false;
-            }
-
             const result = {};
+            
+            const re = /\["[\d]{1,2}",\s"(.*?)",\s/g;
 
-            result.text = `${index + 1}위 ${$(item).text()}`;
-            result.query = `${queryUrl}${encodeURI($(item).text())}`;
+            // console.log(re.exec($(item).text()));
 
             results.push(result);
         });
